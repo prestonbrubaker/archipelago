f5c328ddac922cc493aa2cb343058688c4f8e098
Willoh Robbins <willoh_robbins@outlook.com>
2024-04-06 17:20:39 -0500

    Making a window!

--- archipelago/client_visualizer.py
+++ archipelago/client_visualizer.py
@@ -1,34 +1,48 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QAction, qApp, QMenuBar
 
-class MainApp(QMainWindow):
+class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
-    
-    def initUI(self):
-        # Set the window to full screen
-        self.showFullScreen()
+        self.initializeUI()
+
+    def initializeUI(self):
+        self.setWindowTitle('PyQt5 Fullscreen Toggle')
+        self.setGeometry(100, 100, 600, 400)  # Set default size
         
-        # This central widget will hold the layout for other widgets
-        self.centralWidget = QWidget(self)
+        self.createMenu()
+
+        self.centralWidget = QWidget(self)  # Central widget to hold layout
         self.setCentralWidget(self.centralWidget)
         
-        # Create a vertical layout
-        self.layout = QVBoxLayout(self.centralWidget)
+        layout = QVBoxLayout()  # Use a vertical layout
         
-        # Example: Add a title label to the layout
-        self.title = QLabel("My Evolution Simulation Game", self)
-        self.layout.addWidget(self.title)
+        self.title = QLabel("My Application", self)
+        layout.addWidget(self.title)  # Add a title
         
-        # Placeholder for game area and options
-        # You can add them here using self.layout.addWidget(widget)
+        toggleButton = QPushButton("Toggle Fullscreen", self)
+        toggleButton.clicked.connect(self.toggleFullscreen)
+        layout.addWidget(toggleButton)  # Add a button to toggle fullscreen
         
-        # Set the layout for the central widget
-        self.centralWidget.setLayout(self.layout)
+        self.centralWidget.setLayout(layout)
+
+    def createMenu(self):
+        menuBar = self.menuBar()
+        fileMenu = menuBar.addMenu('File')
+
+        exitAction = QAction('Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.triggered.connect(qApp.quit)
+        fileMenu.addAction(exitAction)
+
+    def toggleFullscreen(self):
+        if self.isFullScreen():
+            self.showNormal()
+        else:
+            self.showFullScreen()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mainApp = MainApp()
+    window = MyApp()
+    window.show()
     sys.exit(app.exec_())
