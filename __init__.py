dfe413229d215e3961d3070091d3fb1efe60b292
Willoh Robbins <willoh_robbins@outlook.com>
2024-04-06 16:28:25 -0500

    adding global init file

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#This is a global __init__ file. It will serve to allow for the global 
+#importation of our modules which may be used across our applications. 
+#This is especially important as we consider the division of scripts. It 
+#allows us to work independently and more efficiently within the same file. 
+#Keeps things better organized. Cleaner structure. This aside, good practice. 
+#I LOVE YOU, PRESTON SCOTT!
