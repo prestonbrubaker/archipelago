32eae70e703a6c4bbc9177b19eca5a2a69882e85
Willoh Robbins <willoh_robbins@outlook.com>
2024-04-10 20:29:22 -0500

    spacing alterations

--- archipelago/title_frame.py
+++ archipelago/title_frame.py
@@ -26,18 +26,18 @@ class MyApp(QMainWindow):
 
         buttonLayout = QHBoxLayout()
         buttonLayout.addWidget(self.menuButton, 0, Qt.AlignRight | Qt.AlignTop)  # Button does not align right
-        buttonLayout.addItem(QSpacerItem(10, 10, QSizePolicy.Expanding, QSizePolicy.Minimum))
+        buttonLayout.addItem(QSpacerItem(10, 10, QSizePolicy.Minimum, QSizePolicy.Expanding))
 
         mainLayout.addLayout(buttonLayout)
 
         self.title = QLabel("Archipelago", self)
         self.title.setAlignment(Qt.AlignCenter)  
-        font = QFont("Arial", 36)
+        font = QFont("Arial", 54)
         self.title.setFont(font)
         self.title.setStyleSheet("color: black; margin-top: 2rem;")  # Font color and top margin
         mainLayout.addWidget(self.title)
 
-        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
         mainLayout.addItem(spacer)
 
         self.centralWidget.setLayout(mainLayout)
