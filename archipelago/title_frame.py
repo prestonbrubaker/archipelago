f4093edadd815dfd0a5d1f079befaf817a753c0c
Willoh Robbins <willoh_robbins@outlook.com>
2024-04-10 20:38:20 -0500

    sad tummy

--- archipelago/title_frame.py
+++ archipelago/title_frame.py
@@ -20,7 +20,7 @@ class MyApp(QMainWindow):
         mainLayout = QVBoxLayout(self.centralWidget) 
 
         self.menuButton = QPushButton("X", self)
-        self.menuButton.setFont(QFont("Arial", 18))
+        self.menuButton.setFont(QFont("Helvetica", 18))
         self.menuButton.clicked.connect(self.close)
 
         buttonLayout = QHBoxLayout()
@@ -33,7 +33,7 @@ class MyApp(QMainWindow):
         self.title.setAlignment(Qt.AlignCenter)  
         font = QFont("Arial", 54)
         self.title.setFont(font)
-        self.title.setStyleSheet("color: black; margin-top: 2rem;")  # Font color and top margin
+        self.title.setStyleSheet("color: black; margin-top: 2rem;")
         mainLayout.addWidget(self.title)
 
         spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -41,10 +41,9 @@ class MyApp(QMainWindow):
 
         self.centralWidget.setLayout(mainLayout)
 
-        
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyApp()
     window.show()
     sys.exit(app.exec_())
+
