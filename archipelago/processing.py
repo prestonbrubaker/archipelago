2ea229fdaeeb3f404498fdcee3a08e78c5581067
Willoh Robbins <willoh_robbins@outlook.com>
2024-04-13 12:26:03 -0500

    please read

--- archipelago/processing.py
+++ archipelago/processing.py
@@ -1,2 +1,6 @@
 #This program is meant to interpret the data passed from the backend and package for visuals
 
+from client_backend import zaza, baba, chacha
+
+#This creates a pipeline of information from client_backend to processing.py
+#The three variables passed represent the node type (values 1, 2, 3), x-type (Nobody knows. Will resume when Preston grows up and discusses his approach with me.) 
