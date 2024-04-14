2c816a9ee8e41a4fa841925671d23bb42689962f
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-04-14 15:44:53 -0500

    Update client_backend.py

--- archipelago/client_backend.py
+++ archipelago/client_backend.py
@@ -675,7 +675,7 @@ def main_loop():
         light = world_light_values[i][j]
         if(light < light_max):
           r = random.uniform(0, 1)
-          if( r < 0.9):
+          if( r < 0.1):
             light += 1
         world_light_values[i][j] = light
           
