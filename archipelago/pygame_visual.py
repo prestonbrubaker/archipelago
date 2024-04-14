e16929bedcd4f1393f2f2e63ebfda85afc2e4b1d
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-04-13 21:33:34 -0500

    Update pygame_visual.py

--- archipelago/pygame_visual.py
+++ archipelago/pygame_visual.py
@@ -12,12 +12,14 @@ while True:
   with open('locations.txt', 'r') as file:
       data_string = file.read()
   data_array = ast.literal_eval(data_string)
-  window.fill((0, 0, 0))
-  for i in range(0, len(data_array)):
-    type = data_array[i][0]
-    x_unit = data_array[i][1]
-    y_unit = data_array[i][2]
-    x = x_unit * 800
-    y = y_unit * 800
-    pygame.draw.rect(window, (255, 0, 0), (x, y, 5, 5))
-  pygame.display.flip()
+  if(len(data_array) != 0):
+    window.fill((0, 0, 0))
+    for i in range(0, len(data_array)):
+      type = data_array[i][0]
+      x_unit = data_array[i][1]
+      y_unit = data_array[i][2]
+      x = x_unit * 800
+      y = y_unit * 800
+      pygame.draw.rect(window, (255, 0, 0), (x, y, 5, 5))
+    pygame.display.flip()
+  time.sleep(1)
