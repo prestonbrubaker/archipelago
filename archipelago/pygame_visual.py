a715c1504665b8a36f0a89e0159922334ed309fa
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-04-13 21:32:02 -0500

    Update pygame_visual.py

--- archipelago/pygame_visual.py
+++ archipelago/pygame_visual.py
@@ -14,9 +14,9 @@ while True:
   data_array = ast.literal_eval(data_string)
   window.fill((0, 0, 0))
   for i in range(0, len(data.array)):
-    type = data.array[i][0]
-    x_unit = data.array[i][1]
-    y_unit = data.array[i][2]
+    type = data_array[i][0]
+    x_unit = data_array[i][1]
+    y_unit = data_array[i][2]
     x = x_unit * 800
     y = y_unit * 800
     pygame.draw.rect(window, (255, 0, 0), (x, y, 5, 5))
