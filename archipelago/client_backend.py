e6e2415e33883e25f3183746b350f020a6ddfcfc
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-04-26 18:12:48 -0500

    Update client_backend.py

--- archipelago/client_backend.py
+++ archipelago/client_backend.py
@@ -418,7 +418,7 @@ def import_organisms():
       else:
         raise ValueError("The content of the file is not a list.")
     except SyntaxError as e:
-          #print(f"Error: {e}")
+          print(f"Error: {e}")
   
       
 
